99419604d52e3e5f7c477438936ed47d4664f29f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-04-17 09:29:37 -0500

    Refactor secret words handling in sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 //Global Variables
 let myInput,myButton,myImage;
-let secretWords;
+let arrayOfSecretWords,secretWord;
 
 function selectSecretWord(){
-  let randomNumber = Math.random();
-  print(randomNumber)
+  let randomNumber = Math.floor(Math.random()*arrayOfSecretWords.length);
+  secretWord = arrayOfSecretWords[randomNumber]
+  print(secretWord)
 }
 function setup() {
   createCanvas(400, 400);
   myInput = createInput()
   myInput.position(20,50)
-  secretWords = ["jazz","lynx","You can't handle the truth","Very Nice","Mark it zero","May the force be with you" ];
+  arrayOfSecretWords = ["jazz","lynx","You can't handle the truth","Very Nice","Mark it zero","May the force be with you" ,"math", "computer science"];
   selectSecretWord();
 }
 function draw() {

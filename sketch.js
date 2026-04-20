688bbfd3e8e7b41fb612760fed679ee9af1cebd1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-04-20 09:03:57 -0500

    Implement makeBlanks function and update secret words
    Added a function to create blanks for the secret word and updated the list of secret words.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 //Global Variables
 let myInput,myButton,myImage;
-let arrayOfSecretWords,secretWord;
+let arrayOfSecretWords,secretWord, solution;
 
+function makeBlanks(){
+  for(let i=0 ; i< secretWord.length; i++){
+    if(secretWord.charAt(i) == " "){
+      solution += " ";
+    }else{
+      solution += "-"
+    }
+  }
+}
 function selectSecretWord(){
   let randomNumber = Math.floor(Math.random()*arrayOfSecretWords.length);
   secretWord = arrayOfSecretWords[randomNumber]
@@ -11,11 +20,17 @@ function setup() {
   createCanvas(400, 400);
   myInput = createInput()
   myInput.position(20,50)
-  arrayOfSecretWords = ["jazz","lynx","You can't handle the truth","Very Nice","Mark it zero","May the force be with you" ,"math", "computer science"];
+  arrayOfSecretWords = ["jazz","lynx","You can't handle the truth","Very Nice","Forest Gump","May the force be with you" ,"math", "computer science"];
   selectSecretWord();
+  solution = "";
+  makeBlanks();
 }
 function draw() {
   background(220);
   text("Guess the Word" ,20,20)
+  push()
+    textSize(60)
+    text(solution, 20,100);
+  pop()
   
 }
